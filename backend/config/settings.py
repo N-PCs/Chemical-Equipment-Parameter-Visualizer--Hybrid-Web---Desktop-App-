ce461f87ae154267544073fc57e7b56472c846f4
Neel Pandey <neelpandeyofficial@gmail.com>
2026-02-05 23:22:43 +0530

    update 9.0.3

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
+DEBUG = os.getenv('DEBUG').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app', '*']
 
